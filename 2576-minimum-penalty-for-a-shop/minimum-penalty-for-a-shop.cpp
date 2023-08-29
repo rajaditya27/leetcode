ee00e26ec2f1e015e03a6fe7de2573c43ff0295a
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2023-08-29 22:17:11 +0530

    Time: 24 ms (80.18%) | Memory: 10.8 MB (72.59%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.cpp
@@ -5,17 +5,10 @@ public:
         
         int minHour  = 0;
         int penalty  = count(begin(customers), end(customers), 'Y');
-        
         int minPenalty = penalty;
-
         for(int i = 0; i< n ; i++) {
-            
-            if(customers[i] == 'Y') {
-                penalty--;
-            } else {
-                penalty++;
-            }
-            
+            if(customers[i] == 'Y')  penalty--;
+            else  penalty++;
             if(penalty < minPenalty) {
                 minHour = i+1;
                 minPenalty = penalty;
